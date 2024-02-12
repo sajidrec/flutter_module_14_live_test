2d95b4ee0417137e57f5613cb6681fb55ab7214b
MD. Sajid Hossain <sajidrec@gmail.com>
2024-02-12 23:35:49 +0600

    typo fixed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_moduel_14_livetest/weather_info_json.dart';
 
@@ -37,29 +35,29 @@ class _HomePageState extends State<HomePage> {
           itemBuilder: (context, index) {
             return Card(
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       "City: ${weatherDataList[index]["city"]}",
-                      style: TextStyle(fontSize: 18),
+                      style: const TextStyle(fontSize: 18),
                     ),
                     Text(
-                      "Tempurature: ${weatherDataList[index]["temperature"]}°C",
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                      "Temperature: ${weatherDataList[index]["temperature"]}°C",
+                      style: const TextStyle(color: Colors.grey, fontSize: 16),
                     ),
                     Text(
                       "Condition: ${weatherDataList[index]["condition"]}",
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                      style: const TextStyle(color: Colors.grey, fontSize: 16),
                     ),
                     Text(
                       "Humidity ${weatherDataList[index]["humidity"]}%",
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                      style: const TextStyle(color: Colors.grey, fontSize: 16),
                     ),
                     Text(
-                      "WindSpeed ${weatherDataList[index]["windSpeed"]} m/s",
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                      "Wind Speed ${weatherDataList[index]["windSpeed"]} m/s",
+                      style: const TextStyle(color: Colors.grey, fontSize: 16),
                     ),
                   ],
                 ),
